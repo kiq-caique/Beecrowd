6b9ef02c95ee4aa179e3652acc3cc79320db237e
kiq-caique <caique-kiq@hotmail.com>
2023-05-23 13:21:31 -0300

    demonstrando git e variaveis pro felipe

--- 1_lista_02.js
+++ 1_lista_02.js
@@ -1,8 +1,8 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var input = require('fs').readFileSync('stdin', 'utf8');
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var input = require("fs").readFileSync("stdin", "utf8");
 
 const pi = 3.14159;
 let raio = parseFloat(input);
-let area = pi * Math.pow(raio,2);
+let area = pi * Math.pow(raio, 2);
 
 console.log("A=" + area.toFixed(4));
